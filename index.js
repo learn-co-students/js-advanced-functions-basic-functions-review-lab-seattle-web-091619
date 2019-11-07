302b9a4ddee27539da1768e890267709d9feb4b4
Packmanager9 <ColeJDitzler@gmail.com>
2019-11-06 16:07:57 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
 // Your code here
+
+
+
+function saturdayFun(toDo = "roller-skate"){
+
+    return `This Saturday, I want to ${toDo}!`
+}
+
+function mondayWork(toDo = "go to the office"){
+
+
+    return `This Monday, I will ${toDo}.`
+}
+
+function wrapAdjective(star ="*"){
+
+      return  function Wowah(adj){
+        return `You are ${star}${adj}${star}!`
+    }
+
+
+
+}
+
+let Calculator = {
+    add(num1, num2){
+
+        return num1+num2
+    },
+    subtract(num1, num2){
+
+        return num1-num2
+    },
+    multiply(num1, num2){
+
+        return num1*num2
+    },
+    divide(num1, num2){
+
+        return num1/num2
+    }
+
+
+
+}
+
+function actionApplyer(int, arr){
+
+    for (let i =0; i< arr.length;i++){
+
+       int =  arr[i](int)
+
+    }
+return int
+}
